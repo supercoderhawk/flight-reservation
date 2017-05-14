a46751fcf93570a18e0249a1d06c2ef05a005359
supercoderhawk <supercoderhawk@gmail.com>
2017-05-14 15:07:47 +0800

    add some util functions

--- src/main/java/com/airline/utils/util.java
+++ src/main/java/com/airline/utils/util.java
@@ -1,24 +1,36 @@
 package com.airline.utils;
 
-import com.airline.DataSource;
 import com.google.gson.Gson;
 
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.nio.charset.Charset;
-import java.nio.file.Files;
-import java.nio.file.Paths;
+import java.time.*;
+import java.time.format.DateTimeParseException;
+import java.util.Optional;
 import java.util.Scanner;
+import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by airline on 2017/5/11.
+ * 工具类，封装一些常用的函数
  */
 public class util {
-  public static DataSource loadDataFromFile(String path){
-    DataSource dataSource = new DataSource();
+
+  public static <T> T loadFileToObject(String path, Class<T> c) {
     Gson gson = new Gson();
+    String data = loadResourceFile(path);
+    return gson.fromJson(data, c);
+  }
+
+  /**
+   * 读取当前类加载器目录的resources目录下的文件，输出为字符串
+   *
+   * @param path: 文件在resources文件夹下的相对路径
+   * @return: 文件字符串
+   */
+  private static String loadResourceFile(String path) {
     String data = null;
     try {
       String fullPath = util.class.getClassLoader().getResource(path).getFile();
@@ -27,6 +39,80 @@ public class util {
       e.printStackTrace();
       return null;
     }
-    return gson.fromJson(data,DataSource.class);
+    return data;
+  }
+
+  public static boolean isTimeToTerminate(String timeString) {
+    Optional<LocalTime> time = Optional.ofNullable(parseTime(timeString));
+    if (!time.isPresent()) {
+      return false;
+    }
+    Duration duration = Duration.between(LocalTime.now(), time.get());
+    return duration.toHours() < 2;
+  }
+
+  public static LocalTime parseTime(String timeString) {
+    LocalTime time = null;
+    try {
+      time = LocalTime.parse(timeString);
+    } catch (DateTimeParseException e) {
+      System.out.println(e.getCause().getLocalizedMessage());
+      return null;
+    }
+    return time;
+  }
+
+  public static boolean isTimeValidate(String timeString) {
+    String pattern = "\\d{2}:\\d{2}:\\d{2}";
+    Pattern r = Pattern.compile(pattern);
+    Matcher m = r.matcher(timeString);
+    if (m.matches()) {
+      try {
+        LocalTime time = LocalTime.parse(timeString);
+      } catch (DateTimeParseException e) {
+        System.out.println(e.getCause().getLocalizedMessage());
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  public static boolean isDateValidate(String dateString){
+    String pattern = "\\d{4}-\\d{2}-\\d{2}";
+    Pattern r = Pattern.compile(pattern);
+    Matcher m = r.matcher(dateString);
+    if(m.matches()){
+      try{
+        LocalDate date = LocalDate.parse(dateString);
+      }catch (DateTimeParseException e){
+        System.out.println(e.getCause().getLocalizedMessage());
+        return false;
+      }
+      return true;
+    }
+    return false;
+  }
+
+  private static String[] chars = new String[]{"a", "b", "c", "d", "e", "f",
+      "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s",
+      "t", "u", "v", "w", "x", "y", "z", "0", "1", "2", "3", "4", "5",
+      "6", "7", "8", "9", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+      "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V",
+      "W", "X", "Y", "Z"};
+
+  public static String generateUserID() {
+    StringBuilder shortBuffer = new StringBuilder();
+    String uuid = UUID.randomUUID().toString().replace("-", "");
+    for (int i = 0; i < 8; i++) {
+      String str = uuid.substring(i * 4, i * 4 + 4);
+      int x = Integer.parseInt(str, 16);
+      shortBuffer.append(chars[x % 0x3E]);
+    }
+    return shortBuffer.toString();
+  }
+
+  public static long generateOrderID(LocalDateTime time){
+    return time.toInstant(OffsetDateTime.now().getOffset()).getEpochSecond();
   }
 }
