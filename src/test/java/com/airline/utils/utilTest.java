23da5e3b256d8d37ed65bc499108b75aceeae852
supercoderhawk <supercoderhawk@gmail.com>
2017-05-14 23:18:47 +0800

    add unit test of Util

--- src/test/java/com/airline/utils/utilTest.java
+++ src/test/java/com/airline/utils/utilTest.java
@@ -1,13 +1,16 @@
 package com.airline.utils;
 
+import com.airline.DataSource;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.time.LocalDateTime;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 /**
  * Created by airline on 2017/5/13.
@@ -15,7 +18,8 @@ import static org.junit.Assert.assertEquals;
  */
 public class utilTest {
 
-  DateTimeFormatter formatter =  DateTimeFormatter.ofPattern("HH:mm:ss");
+  DateTimeFormatter formatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+
   @Before
   public void setUp() throws Exception {
   }
@@ -23,6 +27,7 @@ public class utilTest {
   @After
   public void tearDown() throws Exception {
   }
+
   @Test
   public void isStartTimeValidate() throws Exception {
 
@@ -32,44 +37,61 @@ public class utilTest {
   public void isStartAndArrivalTimeValidate() throws Exception {
     LocalTime start = LocalTime.now();
     LocalTime end = LocalTime.now().plusHours(1);
-    assertEquals(util.isStartAndArrivalTimeValidate(start.format(formatter),end.format(formatter)),true);
+    //assertEquals(Util.isStartAndArrivalTimeValidate(start.format(formatter), end.format(formatter)), true);
 
   }
 
   @Test
   public void isDateValidate() throws Exception {
+    String date1 = "2017-05-14";
+    assertEquals(Util.isDateValidate(date1),true);
+    String date2 = "20170514";
+    assertEquals(Util.isDateValidate(date2),false);
+    String date3 = "2017-13-30";
+    assertEquals(Util.isDateValidate(date3),false);
   }
 
   @Test
   public void generateUserID() throws Exception {
+    assertEquals(Util.generateUserID().length(), 8);
   }
 
   @Test
   public void generateOrderID() throws Exception {
+    assertNotEquals(Util.generateOrderID(LocalDateTime.now()),null);
+    assertEquals(String.valueOf(Util.generateOrderID(LocalDateTime.now())).length(),10);
   }
 
   @Test
   public void encrypt() throws Exception {
+    String source = "123456";
+    String target = "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92";
+    assertEquals(Util.encrypt(source), target);
   }
 
   @Test
   public void loadFileToObject() throws Exception {
+    DataSource dataSource = Util.loadFileToObject("init.json", DataSource.class);
+    assertNotEquals(dataSource, null);
   }
 
   @Test
   public void isTimeValidate() throws Exception {
+    String str1 = "12:00:11";
+    assertEquals(Util.isTimeValidate(str1), true);
+    String str2 = "12:72:10";
+    assertEquals(Util.isTimeValidate(str2), false);
+    String str3 = "12:777:22";
+    assertEquals(Util.isTimeValidate(str2), false);
   }
 
-  @Test
-  public void loadDataFromFile() throws Exception {
-  }
 
   @Test
   public void isTimeToTerminate() throws Exception {
     String timeString1 = LocalTime.now().plusHours(3).format(formatter);
     String timeString2 = LocalTime.now().plusHours(1).format(formatter);
-    //assertEquals(util.isTimeToTerminate(timeString1),false);
-    //assertEquals(util.isTimeToTerminate(timeString2),true);
+    //assertEquals(Util.isTimeToTerminate(timeString1),false);
+    //assertEquals(Util.isTimeToTerminate(timeString2),true);
 
   }
 
@@ -78,8 +100,8 @@ public class utilTest {
     String timeString1 = "10:10:71";
     LocalTime t = LocalTime.now();
     t = t.minusNanos(t.getNano());
-    assertEquals(util.parseTime(timeString1),null);
-    assertEquals(util.parseTime(t.format(formatter)),t);
+    assertEquals(Util.parseTime(timeString1), null);
+    assertEquals(Util.parseTime(t.format(formatter)), t);
   }
 
 }
